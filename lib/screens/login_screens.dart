bda5ae04140d6ed131c16ad1732494f45304b1fb
kervis vasquez <kervisvasquez24@gmail.com>
2021-11-24 12:02:02 -0400

    agregamos validacion con expresion regular

--- lib/screens/login_screens.dart
+++ lib/screens/login_screens.dart
@@ -43,44 +43,59 @@ class _LoginForm extends StatelessWidget {
       child: Container(
         child: Form(
             //TODO: MANTENER LA REFERENCIA EN EL KEY
+            autovalidateMode: AutovalidateMode.onUserInteraction,
             child: Column(children: [
-          TextFormField(
-            autocorrect: false,
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecorations.authInputDecoration(
-              hintText: 'ejemplo@correo.com',
-              labelText: 'Correo',
-              prefixIcon: Icons.alternate_email_sharp,
-            ),
-          ),
-          SizedBox(height: 30),
-          TextFormField(
-            autocorrect: false,
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecorations.authInputDecoration(
-              hintText: '****',
-              labelText: 'Contraseña',
-              prefixIcon: Icons.lock_outline,
-            ),
-          ),
-          SizedBox(height: 30),
-          MaterialButton(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            disabledColor: Colors.grey,
-            elevation: 0,
-            color: Colors.blue,
-            onPressed: () {},
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 70, vertical: 15),
-              child: Text(
-                'ingresar',
-                style: TextStyle(color: Colors.white),
+              TextFormField(
+                autocorrect: false,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecorations.authInputDecoration(
+                  hintText: 'ejemplo@correo.com',
+                  labelText: 'Correo',
+                  prefixIcon: Icons.alternate_email_sharp,
+                ),
+                validator: (value) {
+                  String pattern =
+                      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                  RegExp regExp = new RegExp(pattern);
+                  return regExp.hasMatch(value ?? '')
+                      ? null
+                      : 'El formato no es correcto';
+                },
               ),
-            ),
-          )
-        ])),
+              SizedBox(height: 30),
+              TextFormField(
+                autocorrect: false,
+                keyboardType: TextInputType.emailAddress,
+                obscureText: true,
+                decoration: InputDecorations.authInputDecoration(
+                  hintText: '****',
+                  labelText: 'Contraseña',
+                  prefixIcon: Icons.lock_outline,
+                ),
+                validator: (value) {
+                  if (value != null && value.length >= 6) return null;
+
+                  return 'La contraseña debe ser mayor a 6 caracteres';
+                },
+              ),
+              SizedBox(height: 30),
+              MaterialButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                disabledColor: Colors.grey,
+                elevation: 0,
+                color: Colors.blue,
+                onPressed: () {},
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 70, vertical: 15),
+                  child: Text(
+                    'ingresar',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              )
+            ])),
       ),
     );
   }
