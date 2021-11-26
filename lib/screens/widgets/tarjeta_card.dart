be8d5fb2753666b19bfabee54fb95aea99bc503b
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-11-26 16:16:14 -0400

    💄 add style in card of details

--- lib/screens/widgets/tarjeta_card.dart
+++ lib/screens/widgets/tarjeta_card.dart
@@ -14,7 +14,10 @@ class TargetaCard extends StatelessWidget {
         // color: Colors.red,
         decoration: _BoxDecoration(),
         child: Stack(
-          children: [BackgroundImage()],
+          children: [
+            BackgroundImage(),
+            ProductDetaild(),
+          ],
         ),
       ),
     );
@@ -34,6 +37,48 @@ class TargetaCard extends StatelessWidget {
   }
 }
 
+class ProductDetaild extends StatelessWidget {
+  const ProductDetaild({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(right: 50),
+      child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+          width: double.infinity,
+          height: 60,
+          decoration: boxDecoration(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "esto es una prueba para entregar los productos",
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              )
+            ],
+          )),
+    );
+  }
+
+  BoxDecoration boxDecoration() {
+    return BoxDecoration(
+        color: Colors.indigo,
+        borderRadius: BorderRadius.only(
+          bottomRight: Radius.circular(25),
+          topLeft: Radius.circular(25),
+        ));
+  }
+}
+
 class BackgroundImage extends StatelessWidget {
   const BackgroundImage({
     Key? key,
@@ -41,13 +86,17 @@ class BackgroundImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 400,
-      child: FadeInImage(
-        placeholder: AssetImage('assets/jar-loading.gif'),
-        image: NetworkImage(
-            'https://ik.imagekit.io/demo/img/image1.jpeg?tr=w-400,h-300'),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(25),
+      child: Container(
+        width: double.infinity,
+        height: 400,
+        child: FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          image: NetworkImage(
+              'https://ik.imagekit.io/demo/img/image1.jpeg?tr=w-400,h-300'),
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
